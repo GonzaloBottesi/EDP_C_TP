5baed5aa8b320673c100a5d20761650cc679435d
GonzaloBottesi <gbottesi@itba.edu.ar>
2024-11-06 23:59:24 -0300

    Desarrollo en SMS

--- SMS.py
+++ SMS.py
@@ -1,8 +1,27 @@
 from Aplicacion import Aplicacion
 from Stack import Stack
+from TP_EDP import Telefono
+import datetime
+
 class SMS (Aplicacion):
     def __init__(self, peso) -> None:
         super().__init__(peso)
         self.bandeja = Stack()
         
-test = SMS('0 K')
+    def sendMessage(self, tel : Telefono):
+        
+        """Arma el paquete de datos que envia a la central
+
+        Returns:
+            Packet: _Paquete de datos con orden [Nro. Origen, Nro. Destino, Fecha y hora, Mensaje]_
+        """        
+        
+        destiny = input('Ingrese el numero del destinatario: ')
+        message = input('Ingrese el mensaje:')
+        
+        packet = [tel.numero , destiny, 'Fecha y hora' , message]
+        
+        return packet
+    
+    def receiveMessage (self, packet):
+        self.bandeja.push(packet)
